47e5fd865aa2d724d3c3ad266d2786f5169c963f
Alex Resnik <resnikal71202@users.noreply.github.com>
2023-12-03 02:17:45 +0100

    02

---
+++ src/02/02_1.py
@@ -0,0 +1,37 @@
+import os
+
+current_dir = os.path.dirname(__file__)
+input = os.path.join(current_dir, 'input.txt')
+ouput = os.path.join(current_dir, 'output.txt')
+sum = 0
+
+
+def break_condition(color, count):
+    """only 12 red cubes, 13 green cubes, and 14 blue cubes"""
+    if color == 'red' and count > 12:
+        return True
+    if color == 'green' and count > 13:
+        return True
+    if color == 'blue' and count > 14:
+        return True
+    return False
+
+
+with open(input) as f:
+    for line in f:
+        game_id = line.split()[1][:-1]
+        game_data = line.split(':')[1]
+        break_flag = False
+        for set in game_data.split(';'):
+            for color_count in set.split(','):
+                count = color_count.split()[0]
+                color = color_count.split()[1]
+                if (break_condition(color, int(count))):
+                    break_flag = True
+                    break
+            if break_flag:
+                break
+        if not break_flag:
+            sum += int(game_id)
+
+print(sum)
---
+++ src/02/02_2.py
@@ -0,0 +1,28 @@
+import os
+
+current_dir = os.path.dirname(__file__)
+input = os.path.join(current_dir, 'input.txt')
+ouput = os.path.join(current_dir, 'output.txt')
+sum = 0
+
+
+with open(input) as f:
+    for line in f:
+        game_id = line.split()[1][:-1]
+        game_data = line.split(':')[1]
+        min_red = 0
+        min_green = 0
+        min_blue = 0
+        for set in game_data.split(';'):
+            for color_count in set.split(','):
+                count = color_count.split()[0]
+                color = color_count.split()[1]
+                if (color == 'red' and int(count) > min_red):
+                    min_red = int(count)
+                if (color == 'green' and int(count) > min_green):
+                    min_green = int(count)
+                if (color == 'blue' and int(count) > min_blue):
+                    min_blue = int(count)
+        sum += min_red * min_green * min_blue
+        
+print(sum)
