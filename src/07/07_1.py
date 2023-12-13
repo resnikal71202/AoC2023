09064e4f50433c3ae7965e84445120344220d2ea
Alex Resnik <resnikal71202@users.noreply.github.com>
2023-12-13 16:53:23 +0100

    07

---
+++ src/07/07_1.py
@@ -0,0 +1,117 @@
+import copy
+import os
+
+current_dir = os.path.dirname(__file__)
+input = os.path.join(current_dir, 'input.txt')
+hands = []
+
+
+class Hand:
+    def __init__(self, bid, cards_str):
+        self.cards = self.create_cards_from_str(cards_str)
+        self.score = 0
+        self.values = []
+        self.bid = bid
+        self.value = 0
+    
+    def create_cards_from_str(self, cards_str):
+        """
+        """
+        cards = []
+        for c in cards_str:
+            if c == 'A':
+                cards.append(hex(14))
+            elif c == 'K':
+                cards.append(hex(13))
+            elif c == 'Q':
+                cards.append(hex(12))
+            elif c == 'J':
+                cards.append(hex(11))
+            elif c == 'T':
+                cards.append(hex(10))
+            else:
+                cards.append(hex(int(c)))
+        return cards
+    
+    def flatten_cards(self):
+        """
+        create a string from the cards, without 0x
+        """
+        cards = ''
+        for card in self.cards:
+            cards += str(card)[2:]
+        return cards
+    
+    def calculate_score(self):
+        """
+        - Five of a kind
+        - Four of a kind
+        - Full house
+        - Three of a kind
+        - Two pair
+        - One pair
+        - High card
+        """
+        self.score = int(self.flatten_cards(), 16)
+        # print(self.score)
+        cards = copy.copy(self.cards)
+        cards.sort(reverse=True)
+        # # print(cards)
+        for card in cards:
+            self.values.append((cards.count(card), card))
+        
+        # remove duplicates
+        self.values = list(set(self.values))
+        # sort by count and value (first by count, then by value)
+        self.values.sort(key=lambda x: (x[0], x[1]), reverse=True)
+        
+        # check for five of a kind
+        if self.values[0][0] == 5:
+            self.score = self.score ** 10000
+        
+        # check for four of a kind
+        if self.values[0][0] == 4:
+            self.score = self.score ** 1000
+        
+        # check for three of a kind
+        if self.values[0][0] == 3:
+            # check for full house
+            if self.values[1][0] == 2:
+                self.score = self.score ** 500
+            else:
+                # three of a kind
+                self.score = self.score ** 100
+        # check for pair
+        if self.values[0][0] == 2:
+            # check for two pair
+            if self.values[1][0] == 2:
+                self.score = self.score ** 50
+            else:
+                # one pair
+                self.score = self.score ** 10
+        # print(self.score)
+
+
+def main():
+    with open(input, 'r') as f:
+        hands_str = f.readlines()
+
+    hands = []
+    for hand_str in hands_str:
+        hand = hand_str.strip()
+        cards_str, bid = hand.split(' ')
+        hand = Hand(bid, cards_str)
+        hand.calculate_score()
+        hands.append(hand)
+
+    # # sort hands by score
+    hands.sort(key=lambda x: x.score)
+    # calculate sum
+    sum = 0
+    for i in range(0, len(hands)):
+        sum += int(hands[i].bid) * (i + 1)
+    print(sum)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/notes.md
@@ -0,0 +1,89 @@
+# Notes
+
+```mermaid
+graph TD
+    A[Requirement Analysis] --> B[Data Acqisition]
+    B --> C[System Desing & Implementation]
+    C --> D[System Refining]
+    D --> C[System Desing & Implementation]
+    D --> E[Product Demonstration]
+```
+
+## Requirement Analysis
+
+- What is the problem?
+- What is the goal?
+- What are the requirements?
+- What are the constraints?
+- What are the risks?
+- What are the assumptions?
+- What are the dependencies?
+- What are the costs?
+- What are the benefits?
+- What are the alternatives?
+- What are the priorities?
+- What are the deadlines?
+- What are the milestones?
+- What are the deliverables?
+
+## Data Acqisition
+
+- What data is needed?
+- What data is available?
+- What data is missing?
+- What data is redundant?
+- What data is inconsistent?
+- What data is erroneous?
+- What data is sensitive?
+- What data is secure?
+- What data is private?
+- What data is public?
+- What data is structured?
+- What data is unstructured?
+
+## System Desing & Implementation
+
+- What is the architecture?
+- What is the design?
+- What is the implementation?
+- What is the testing?
+- What is the deployment?
+- What is the maintenance?
+- What is the support?
+- What is the documentation?
+- What is the monitoring?
+
+## System Refining
+
+- What is the feedback?
+- What is the review?
+- What is the analysis?
+- What is the optimization?
+- What is the improvement?
+- What is the enhancement?
+
+## System Testing
+
+- What is the unit test?
+- What is the integration test?
+- What is the system test?
+- What is the acceptance test?
+- What is the regression test?
+- What is the performance test?
+- What is the stress test?
+- What is the load test?
+- What is the security test?
+- What is the penetration test?
+- What is the usability test?
+- What is the accessibility test?
+- What is the compatibility test?
+
+## Product Demonstration
+
+- What is the presentation?
+- What is the demonstration?
+- What is the feedback?
+- What is the review?
+- What is the analysis?
+- What is the optimization?
+
